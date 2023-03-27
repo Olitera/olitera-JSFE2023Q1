69a3cbbaa96be9fcd7c6074045f1f0d08de93f4e
Olitera <lu_gu@mail.ru>
2023-03-27 14:13:20 +0200

    fead add hover to our-pets page

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,15 +1,37 @@
-const optionDiv = document.querySelector('.option');
-const cardsTextArray = ['Pet food', 'Transportation', 'Toys', 'Bowls and cups', 'Shampoos', 'Vitamins', 'Medicines', 'Collars / leashes', 'Sleeping areas'];
+console.log(``)
 
-const cardInner = `<div class="option-card-foto"></div>
-<h4 class="header-4">Pet food</h4>`;
+const containerDiv = document.querySelector('.slider-inner');
+const cardTextArray = ['Katrine', 'Jennifer', 'Woody'];
 
-for (let i = 0; i < cardsTextArray.length; i++) {
+const cardInner = `<div class="card-foto"></div>
+<h4 class="header-4">Katrine</h4>
+<button class="button">Learn more</button>`;
+
+for (let i = 0; i < cardTextArray.length; i++) {
   const divElement = document.createElement('div');
 
-  divElement.className = `option-card option-card-${i + 1}`;
+  divElement.className = `card card-${i + 1}`;
   divElement.innerHTML = cardInner;
   const textElement = divElement.querySelector('.header-4');
-  textElement.innerHTML = cardsTextArray[i];
-  optionDiv.append(divElement);
+  textElement.innerHTML = cardTextArray[i];
+  containerDiv.append(divElement);
 }
+
+
+const optionDiv = document.querySelector('.option');
+const optionCardTextArray = ['Pet food', 'Transportation', 'Toys', 'Bowls and cups', 'Shampoos', 'Vitamins', 'Medicines', 'Collars / leashes', 'Sleeping areas'];
+
+const optionCardInner = `<div class="option-card-foto"></div>
+<h4 class="header-4">Pet food</h4>`;
+
+for (let i = 0; i < optionCardTextArray.length; i++) {
+  const optionCard = document.createElement('div');
+
+  optionCard.className = `option-card option-card-${i + 1}`;
+  optionCard.innerHTML = optionCardInner;
+  const optionTextElement = optionCard.querySelector('.header-4');
+  optionTextElement.innerHTML = optionCardTextArray[i];
+  optionDiv.append(optionCard);
+}
+
+
--- shelter/pages/our-pets/script.js
+++ shelter/pages/our-pets/script.js
@@ -1,16 +1,18 @@
-const optionDiv = document.querySelector('.card-container');
-const cardsTextArray = ['Katrine', 'Jennifer', 'Woody', 'Sophia', 'Timmy', 'Charly', 'Scarlett', 'Freddie'];
+console.log(``)
+
+const containerDiv = document.querySelector('.card-container-inner');
+const cardTextArray = ['Katrine', 'Jennifer', 'Woody', 'Sophia', 'Timmy', 'Charly', 'Scarlett', 'Freddie'];
 
 const cardInner = `<div class="card-foto"></div>
 <h4 class="header-4">Katrine</h4>
 <button class="button">Learn more</button>`;
 
-for (let i = 0; i < cardsTextArray.length; i++) {
+for (let i = 0; i < cardTextArray.length; i++) {
   const divElement = document.createElement('div');
 
   divElement.className = `card card-${i + 1}`;
   divElement.innerHTML = cardInner;
   const textElement = divElement.querySelector('.header-4');
-  textElement.innerHTML = cardsTextArray[i];
-  optionDiv.append(divElement);
+  textElement.innerHTML = cardTextArray[i];
+  containerDiv.append(divElement);
 }
