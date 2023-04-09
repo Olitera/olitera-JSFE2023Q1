270bb8e071cccd57be4dc1131b7cd7ef8367fa48
Olitera <lu_gu@mail.ru>
2023-04-09 10:28:48 +0200

    fix: construct cards from json

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,24 +1,18 @@
 console.log(``)
 
-const containerDiv = document.querySelector('.slider-inner');
+const body = document.querySelector('body');
+
+// Work input and ancor
 const helpInput = document.getElementById('help-shelter');
-const cardTextArray = ['Katrine', 'Jennifer', 'Woody'];
 
-const cardInner = `<div class="card-foto"></div>
-<h4 class="header-4">Katrine</h4>
-<button class="button">Learn more</button>`;
-
-for (let i = 0; i < cardTextArray.length; i++) {
-  const divElement = document.createElement('div');
-
-  divElement.className = `card card-${i + 1}`;
-  divElement.innerHTML = cardInner;
-  const textElement = divElement.querySelector('.header-4');
-  textElement.innerHTML = cardTextArray[i];
-  containerDiv.append(divElement);
-}
+document.addEventListener('DOMContentLoaded', () => {
+  if (!!location.href.match(/#help/)) {
+    helpInput.checked = true;
+  }
+})
 
 
+// option cards constructor in section help
 const optionDiv = document.querySelector('.option');
 const optionCardTextArray = ['Pet food', 'Transportation', 'Toys', 'Bowls and cups', 'Shampoos', 'Vitamins', 'Medicines', 'Collars / leashes', 'Sleeping areas'];
 
@@ -35,43 +29,45 @@ for (let i = 0; i < optionCardTextArray.length; i++) {
   optionDiv.append(optionCard);
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  if (!!location.href.match(/#help/)) {
-    helpInput.checked = true;
+
+// cards constrictor from json in slider
+const containerDiv = document.querySelector('.slider-inner');
+
+const cardInner = `<div class="card-foto"></div>
+<h4 class="header-4"></h4>
+<button class="button">Learn more</button>`;
+
+fetch('https://raw.githubusercontent.com/rolling-scopes-school/olitera-JSFE2023Q1/shelter-part3/shelter/pages/pets.json?token=GHSAT0AAAAAAB7NUPOIPREQ4HD27UDSYSIUZBSPVSA')
+.then(response => response.json())
+.then ((data)=> {
+  console.log(data[0].name)
+  for (let i = 0; i < data.length; i++) {
+    // console.log(data[i]);
+    const cardElement = document.createElement('div');
+    cardElement.className = `card`;
+    cardElement.innerHTML = cardInner;
+    const textElement = cardElement.querySelector('.header-4');
+    textElement.innerHTML = data[i].name;
+    const fotoElement = cardElement.querySelector('.card-foto');
+    fotoElement.style.backgroundImage = `url('${data[i].img}')`;
+    containerDiv.append(cardElement);
+    cardElement.addEventListener('click', openModal);
   }
 })
 
 
-const menu = document.querySelector('.burger-menu-form');
-const burger = document.querySelector('.burger');
 
-function newMenu() {
-  menu.style.transform = 'translate(0vw)';
-  burger.style.transform = 'rotate(90deg)';
-}
-
-burger.addEventListener('click', newMenu);
-
-function closeMenu() {
-
-  menu.style.transform = '';
-  burger.style.transform = 'rotate(0deg)';
-}
-
-menu.addEventListener('click', closeMenu);
-
-const card = document.querySelector('.card');
 const modal = document.querySelector('.pets-modal');
 const modalClose = document.querySelector('.modal-close');
 
 function openModal() {
   modal.style.transform = 'translate(0vw)';
+  body.style.overflow = "hidden";
 }
-
-card.addEventListener('click', openModal);
 
 function closeMod() {
   modal.style.transform = '';
+  body.style.overflow = "";
 }
 
 modalClose.addEventListener('click', closeMod);
@@ -80,7 +76,34 @@ function closeModal(event) {
   if (event.target === modal) {
     modal.style.transform = '';
   }
+  document.body.style.overflow = "";
 }
 
 modal.addEventListener('click', (event) => closeModal(event));
+
+
+
+// burger menu
+const menu = document.querySelector('.burger-menu-form');
+const burger = document.querySelector('.burger');
+
+function newMenu() {
+  menu.style.transform = 'translate(0vw)';
+  burger.style.transform = 'rotate(90deg)';
+  body.style.overflow = "hidden"
+}
+
+burger.addEventListener('click', newMenu);
+
+function closeMenu() {
+  menu.style.transform = '';
+  burger.style.transform = 'rotate(0deg)';
+  body.style.overflow = "";
+}
+
+menu.addEventListener('click', closeMenu);
+
+
+
+
 
